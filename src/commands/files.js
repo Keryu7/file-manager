dfc744224cc28ad27b4ab7cf55812f5c74f94ae7
Keryu7 <yyurikt@tut.by>
2024-10-11 09:41:46 +0200

    feat: work with files added

--- src/commands/files.js
+++ src/commands/files.js
@@ -17,3 +17,63 @@ export const readFile = async (currentDir, filePath) => {
         console.log('Operation failed: File does not exist.');
     }
 };
+
+export const createFile = async (currentDir, fileName) => {
+    const fullPath = path.resolve(currentDir, fileName);
+    try {
+        await fs.writeFile(fullPath, '');
+        console.log('File created successfully.');
+    } catch (err) {
+        console.log('Operation failed: Unable to create file.');
+    }
+};
+
+export const renameFile = async (currentDir, oldName, newName) => {
+    const oldPath = path.resolve(currentDir, oldName);
+    const newPath = path.resolve(currentDir, newName);
+    try {
+        await fs.rename(oldPath, newPath);
+        console.log('File renamed successfully.');
+    } catch (err) {
+        console.log('Operation failed: Unable to rename file.');
+    }
+};
+
+export const copyFile = async (currentDir, src, dest) => {
+    const srcPath = path.resolve(currentDir, src);
+    const destPath = path.resolve(currentDir, dest);
+    const readStream = createReadStream(srcPath);
+    const writeStream = createWriteStream(destPath);
+
+    readStream.pipe(writeStream);
+    writeStream.on('finish', () => console.log('File copied successfully.'));
+    writeStream.on('error', () => console.log('Operation failed: Unable to copy file.'));
+};
+
+export const moveFile = async (currentDir, src, dest) => {
+    const srcPath = path.resolve(currentDir, src);
+    const destPath = path.resolve(currentDir, dest);
+    const readStream = createReadStream(srcPath);
+    const writeStream = createWriteStream(destPath);
+
+    readStream.pipe(writeStream);
+    writeStream.on('finish', async () => {
+        try {
+            await fs.unlink(srcPath);
+            console.log('File moved successfully.');
+        } catch (err) {
+            console.log('Operation failed: Unable to move file.');
+        }
+    });
+    writeStream.on('error', () => console.log('Operation failed: Unable to copy file.'));
+};
+
+export const deleteFile = async (currentDir, filePath) => {
+    const fullPath = path.resolve(currentDir, filePath);
+    try {
+        await fs.unlink(fullPath);
+        console.log('File deleted successfully.');
+    } catch (err) {
+        console.log('Operation failed: Unable to delete file.');
+    }
+};
